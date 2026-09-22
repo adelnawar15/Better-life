9155c340108353e6cd25ce7e04c4fa5666cdd92d
adelnawar15 <44569571+adelnawar15@users.noreply.github.com>
2026-09-23 02:01:39 +0300

    Activate Better Life PWA updates immediately

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='better-life-v8';
-self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg','./mountains.svg','./apple-touch-icon.png','./icon-192.png','./icon-512.png']))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(x=>x.startsWith('better-life-')&&x!==C).map(x=>caches.delete(x))))));
+const C='better-life-v9';
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg','./mountains.svg','./apple-touch-icon.png','./icon-192.png','./icon-512.png'])).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(x=>x.startsWith('better-life-')&&x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==self.location.origin)return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
