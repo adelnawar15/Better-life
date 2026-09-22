6a47295620fff179291c046171ae29292c00d615
adelnawar15 <44569571+adelnawar15@users.noreply.github.com>
2026-09-22 19:14:10 +0300

    Expand Better Life into a connected life OS

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='better-life-v7';
+const C='better-life-v8';
 self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon.svg','./mountains.svg','./apple-touch-icon.png','./icon-192.png','./icon-512.png']))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(x=>x.startsWith('better-life-')&&x!==C).map(x=>caches.delete(x))))));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET'||new URL(e.request.url).origin!==self.location.origin)return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
